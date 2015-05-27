c9faa44ecebde9e99964bec06f813437fdc53669
Haneef Abdul Karim <haneefx7@gmail.com>
2015-05-27 12:55:30 +0800

    Modified app.js

--- js/app.js
+++ js/app.js
@@ -1,29 +1,5 @@
-function getName() {
-    // 'use strict';
+var confess = function (name) {
+    'use strict';
     
-    console.log(this);
-    
-    this.name = 'Putri';
-}
-
-getName();
-
-var a = {
-    name: 'Tira',
-    log: function () {
-        // 'use strict';
-        console.log(this);
-        
-        var self = this;
-        
-        var changeName = function (newName) {
-            self.name = newName;
-        };
-        
-        changeName('Putri Nurzarith Atirah');
-        
-        console.log(self);
-    }
-};
-
-a.log();
+    console.log('I love you, ' + name);
+}('Putri');
