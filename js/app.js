b2d5458dbbf8833a9c085398d32993044a55630d
Haneef Abdul Karim <haneefx7@gmail.com>
2015-05-30 12:55:53 +0800

    Modified app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-var confess = function (name) {
+var person = {
+    firstName: 'Tira',
+    lastName: 'Zainal',
+    getFullName: function () {
+        'use strict';
+        
+        return this.firstName + ' ' + this.lastName;
+    }
+};
+
+var personTwo = {
+    firstName: 'Putri Nurzarith Atirah',
+    lastName: 'Megat Zainal'
+};
+
+var log = function () {
     'use strict';
-    
-    console.log('I love you, ' + name);
-}('Putri');
+    console.log('Last accessed by: ' + this.getFullName());
+};
+
+log.call(person);
+console.log(person.getFullName.call(personTwo));
