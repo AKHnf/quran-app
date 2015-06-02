e549c86c690dab8a7c396fbf64454751e14e7e4e
Haneef Abdul Karim <haneefx7@gmail.com>
2015-06-02 13:26:43 +0800

    Modified index.html and app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,5 @@
-var person = {
-    firstName: 'Tira',
-    lastName: 'Zainal',
-    getFullName: function () {
-        'use strict';
-        
-        return this.firstName + ' ' + this.lastName;
-    }
-};
-
-var personTwo = {
-    firstName: 'Putri Nurzarith Atirah',
-    lastName: 'Megat Zainal'
-};
-
-var log = function () {
+(function (window, document, undefined) {
     'use strict';
-    console.log('Last accessed by: ' + this.getFullName());
-};
-
-log.call(person);
-console.log(person.getFullName.call(personTwo));
+    
+    
+}(window, document));
